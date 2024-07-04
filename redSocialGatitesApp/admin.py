15b932dc5dc8dcc3d04ee6fdbd2134fb9dc2b328
Pablo Lopez <pablo.lpzh@gmail.com>
2024-07-04 12:41:35 -0400

    actualizacion del archivo models py

--- redSocialGatitesApp/admin.py
+++ redSocialGatitesApp/admin.py
@@ -3,8 +3,6 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Imagen)
-
 admin.site.register(Publicacion)
 
 admin.site.register(Perfil_usuario)
--- redSocialGatitesApp/models.py
+++ redSocialGatitesApp/models.py
@@ -3,29 +3,23 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Imagen(models.Model):
-    url_imagen = models.CharField(max_length=200)
-    fecha = models.DateTimeField("date published")
-    id_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self) -> str:
-        return self.url_imagen
-
-
 class Publicacion(models.Model):
     texto = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=200, default='titulo por defecto')
     fecha = models.DateTimeField("date published")
     fecha_actualizacion = models.DateTimeField(null = True)
-    id_imagen = models.ForeignKey(Imagen, on_delete=models.CASCADE)
+    url_imagen = models.TextField(null=True, blank=True)
+    nombre_imagen = models.TextField(null=True, blank=True)
     id_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self) -> str:
-        return f'publicacion del usuario {self.id_usuario}' 
+        return f'{self.titulo}' 
 
 
 class Perfil_usuario(models.Model):
     id_usuario= models.OneToOneField(User, on_delete=models.CASCADE)
-    id_imagen= models.ForeignKey(Imagen, on_delete=models.CASCADE)
+    url_imagen = models.TextField(null=True, blank=True)
+    nombre_imagen = models.TextField(null=True, blank=True)
     descripcion = models.CharField(max_length=200)
 
     def __str__(self) -> str:
@@ -40,7 +34,7 @@ class Corazon(models.Model):
 
 class Comentario(models.Model):
     id_usuario= models.ForeignKey(User, on_delete=models.CASCADE)
-    id_publicacion= models.ForeignKey(Imagen, on_delete=models.CASCADE)
+    id_publicacion= models.ForeignKey(Publicacion, on_delete=models.CASCADE)
     texto = models.CharField(max_length=200)
 
     def __str__(self) -> str:
